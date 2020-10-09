2d23895e0ec429c544a48f800d1dd50a40263c77
Sang won <dnjs2618@naver.com>
2020-10-09 12:37:59 +0900

    Updates

---
+++ precache-manifest.1a825a5739bd281862af61ecd4b73656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5784558e0b111d449eac77f8c193b5",
+    "url": "/boxoffice-movie-api/index.html"
+  },
+  {
+    "revision": "c0c5b773e9fc0378472a",
+    "url": "/boxoffice-movie-api/static/js/2.e19cf0c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boxoffice-movie-api/static/js/2.e19cf0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1367542b0e7d3c56743",
+    "url": "/boxoffice-movie-api/static/js/main.fe91cdce.chunk.js"
+  },
+  {
+    "revision": "b85f099154668cf8ab50",
+    "url": "/boxoffice-movie-api/static/js/runtime-main.2a3f59db.js"
+  }
+]);
